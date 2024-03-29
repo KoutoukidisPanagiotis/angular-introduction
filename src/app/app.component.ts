e96e0f219fe55459b3b309690a9d2c7ea09f0f0e
Koutoukidis Panagiotis <koutoukidis_panagiotis@hotmail.com>
2024-03-29 19:02:13 +0200

    one way binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,9 +3,16 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   standalone: true,
+  imports:[],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'angular-introduction';
+  name = 'Panagiotis';
+  person = {
+    givenName: 'Panagiotis',
+    surName: 'Koutoukidis',
+    age: 27,
+    email: 'koutoukidis_panagiotis@hotmail.com',
+  }
 }
